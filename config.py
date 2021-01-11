535085cd464a19f9e7a23c9d590e47bd3cdc565a
Antonio Jesús García Nieto <45717153+AntonioJesusGarciaNieto@users.noreply.github.com>
2021-01-11 19:09:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+from boto.s3.connection import S3Connection
+
+#===Secret======
+
+TOKEN = S3Connection(os.environ['TOKEN'], os.environ['S3_SECRET'])
+
+#=====DECIDE-HEROKU========
 BASE_URL_HEROKU = "https://decide-voting.herokuapp.com/"
 API_BASE = "gateway/"
 
 #=======LOCAL=============
 API_DECIDE = 'http://localhost:8000/'
-TOKEN = 'Njg5MDc5MTUzODMxODM3NzE2.Xm9o-A.t7pODcv5DWNiXm-V6WcMDjAyMTc'
+
 #=======HEROKU============
 PORT = '5000'
 HEROKU_APP_NAME = 'decide-cabina-bot-discord'
