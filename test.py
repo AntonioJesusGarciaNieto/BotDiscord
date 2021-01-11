baecbec9b3296e90a670d66f5284b4f8d25e4773
Antonio Jesús García Nieto <45717153+AntonioJesusGarciaNieto@users.noreply.github.com>
2021-01-11 18:03:57 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,8 +12,11 @@ class TestMethods(unittest.TestCase):
     def test_login(self):
 
         credentials = {"username": USER_1_USER, "password": USER_1_PASS}
-
-        r = requests.post(config.BASE_URL_HEROKU +  config.API_BASE + "authentication/login/", credentials)
+        
+        consulta = "authentication/login/"
+        url = config.BASE_URL_HEROKU + config.API_BASE + consulta
+        
+        r = requests.post(url, credentials)
         
         print(r.status_code)
         
