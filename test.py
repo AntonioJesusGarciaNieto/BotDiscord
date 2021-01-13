4a1fb764d6b7d19d71df33fa7b14ff9ce6708ba8
Antonio Jesús García Nieto <curiosidadcompacta@gmail.com>
2021-01-13 19:53:04 +0100

    Editado test.py
    Añadido Token de usuario

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import config
 USER_1_USER = "user"
 USER_1_PASS = "rinoceronte2"
 TOKEN = config.TOKEN
+USER_1_TOKEN = "f016cd06e314c5f02c14f7408329067a4cd92bc0"
 
 class TestMethods(unittest.TestCase):
 
@@ -23,9 +24,9 @@ class TestMethods(unittest.TestCase):
 
 
     def test_votings(self):
+ 
+        headers = {"token": str(USER_1_TOKEN)}
 
-        
-        headers = {"token": str(TOKEN)}
         consulta = "votings/"
         url = config.BASE_URL_HEROKU + config.API_BASE + consulta
         
