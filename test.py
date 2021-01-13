fe931f2ecbcfb868dede16fe94e0ed77ae362e8c
Antonio Jesús García Nieto <curiosidadcompacta@gmail.com>
2021-01-13 19:43:04 +0100

    Creado test_votings y editado test_login

--- test.py
+++ test.py
@@ -6,21 +6,32 @@ import config
 
 USER_1_USER = "user"
 USER_1_PASS = "rinoceronte2"
+TOKEN = config.TOKEN
 
 class TestMethods(unittest.TestCase):
 
     def test_login(self):
 
-        #credentials = {"username": USER_1_USER, "password": USER_1_PASS}
+        credentials = {"username": USER_1_USER, "password": USER_1_PASS}
         
-        #consulta = "authentication/login/"
-        #url = config.BASE_URL_HEROKU + config.API_BASE + consulta
+        consulta = "authentication/login/"
+        url = config.BASE_URL_HEROKU + config.API_BASE + consulta
         
-        #r = requests.post(url, credentials)
+        r = requests.post(url, credentials)
         
-        #print(r.status_code)
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_votings(self):
+
         
-        self.assertEqual(200, 200)
+        headers = {"token": str(TOKEN)}
+        consulta = "votings/"
+        url = config.BASE_URL_HEROKU + config.API_BASE + consulta
+        
+        r = requests.get(url, headers = headers)
+        
+        self.assertEqual(r.status_code, 200)
 
 
 if __name__ == '__main__':
